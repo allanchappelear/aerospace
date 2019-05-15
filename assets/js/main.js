e4a362c97c5c4545ba98598f6b052044c34e29be
Allan Chappelear <allanc@newtarget.com>
2019-05-15 15:43:08 -0400

    updated scroll fade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,24 @@
  var scene = document.getElementById('scene');
  var parallax = new Parallax(scene);
-$('#header1').hide();
-$('#header2').hide();
-jQuery(document).ready(function($){
-	
-	$('#header1').fadeIn();
-	$(window).scroll(function() {
-	var scroll = $(window).scrollTop();
- 	var windowHeight = $(window).height();
-	$("#header1").css({
-		transform: 'translate3d(0%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll/5)/100+')',
+ $('#header1').hide();
+ $('#header2').hide();
+ jQuery(document).ready(function($){
+
+ 	$('#header1').fadeIn();
+ 	$(window).scroll(function() {
+ 		var scroll = $(window).scrollTop();
+ 		var windowHeight = $(window).height();
+ 		$("#header1").css({
+ 			transform: 'translate3d(0%, -'+(scroll/150)+'%, 0) scale('+(150 + scroll/5)/150+')',
 		//Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
 		"-webkit-filter": "blur(" + (scroll/200) + "px)",
 		filter: "blur(" + (scroll/200) + "px)",
-		opacity: (100/scroll)
+		opacity: (120/scroll)
 	});
-	if(scroll > (windowHeight - (windowHeight/2))) {
-		$('#header2').fadeIn();
-	} else {
-		$('#header2').fadeOut();
-	}
-	
-	});
-
-
-
-});
+ 		if(scroll > (windowHeight - (windowHeight/2) + 200)) {
+ 			$('#header2').fadeIn();
+ 		} else {
+ 			$('#header2').fadeOut();
+ 		}
+ 	});
+ });
